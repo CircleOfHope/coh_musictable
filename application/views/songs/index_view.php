088aa8abbe207a44a0a1314c5fab835c03eaf31e
josh <josh@tritium>
2014-11-12 22:12:52 -0500

    Add welcome message to homepage.
    https://trello.com/c/j1W7fSj7

--- application/views/songs/index_view.php
+++ application/views/songs/index_view.php
@@ -1,3 +1,11 @@
+<a onclick="$('div#inline_howto').removeAttr('hidden')">Are you new here? Click me!</a>
+<div id="inline_howto" hidden="hidden">
+Welcome to the Music Table! There are a few things you should know about the Table and how to use it:<br />
+<br />
+1) You can choose between English and Non-English or Both for searching.<br />
+2) You can search by keywords. Some of the main keywords we use are: Hymn, Declarative, Gospel, Psalm, Christmas, and Lent.<br />
+3) Report issues/fixes to <a href="mailto:luke&commat;circleofhope.net">luke&commat;circleofhope.net</a><br />
+</iframe></div>
 <h1>Music Database</h1>
 <form id="index_search_form" action="<?=site_url('songs/index')?>" method="post">
 	<div>
