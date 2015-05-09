2adc23fcf876ae481009342c202c4a22058ee59e
josh <josh@tritiumoxide.net>
2015-05-08 21:28:08 -0400

    update to use fulltext indices for search, no longer using stored procedure, also fix CI config issue

--- application/models/song.php
+++ application/models/song.php
@@ -38,8 +38,29 @@ class Song extends DataMapper {
         $page_index = isset($options['page_index']) ? $options['page_index'] : 0;
         $search_string = $options['search_string'];
         $language = $options['language'];
-        $qstring = 'CALL SearchSongs(?, ?);';
-        return $this->db->query($qstring, array($search_string, $language));
+	$qstring = "
+SELECT distinct s.*,
+MATCH(s.Title, s.Artist, s.Scripture, s.LyricsExcerpt, s.Notes) AGAINST ('$search_string' in boolean mode) as score
+FROM songs s
+    LEFT JOIN songs_tags st ON st.song_id = s.id
+    LEFT JOIN tags t ON t.id = st.tag_id
+    LEFT JOIN tags_tagtypes tt ON tt.id = st.tag_id
+WHERE
+    (
+        '$language' = ''
+        OR $language = 1 AND t.name = 'English'
+        OR $language = 2 AND t.name = 'Foreign'
+        OR $language = 3
+    )
+    AND
+    (
+    (
+        '$search_string' = '' OR MATCH(s.Title, s.Artist, s.Scripture, s.LyricsExcerpt, s.Notes) AGAINST ('$search_string' in boolean mode)
+    )
+    OR t.Name LIKE '%{$search_string}%'
+    )
+ORDER BY score DESC, s.Title ASC;";
+        return $this->db->query($qstring);
     }
 
     function add($data)
