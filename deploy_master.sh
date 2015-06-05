8a3bf61bf229a7b04b28edebc037bf91dda391bf
josh <josh@tritiumoxide.net>
2015-06-04 20:45:59 -0400

    make changes for dreamhost move

--- deploy_master.sh
+++ deploy_master.sh
@@ -15,21 +15,24 @@ git checkout master
 git pull --ff-only
 
 #make some server-only tweaks
+perl -pi -e "s#(\[\'hostname\'\] = )\'\S+\'#\1\'mysql.musiccircleofhope.dreamhosters.com\'#" public_html/application/config/database.php
+perl -pi -e "s#(\[\'username\'\] = )\'\S+\'#\1\'circlemusic\'#" public_html/application/config/database.php
 perl -pi -e "s#(\[\'password\'\] = )\'\S+\'#\1\'$1\'#" public_html/application/config/database.php
+perl -pi -e "s#(\[\'database\'\] = )\'\S+\'#\1\'circlemusic\'#" public_html/application/config/database.php
 perl -pi -e "s#'ENVIRONMENT', 'development'#'ENVIRONMENT', 'production'#" public_html/index.php
 
 #backup database and files
-mysqldump -u music -p$1 music --routines > musictable.sql
-tar cfz ../backup/$timestamp.tar.gz public_html musictable.sql
+mysqldump -u circlemusic -p$1 circlemusic --routines > musictable.sql
+tar cfz ../backup/$timestamp.tar.gz music.circleofhope.net musictable.sql
 rm musictable.sql
 
 echo "New website is staged, press Return to move it into production, otherwise Ctrl-C to abort."
 read
 
 #install new directories
-mv ../public_html ../public_html.old
-mv public_html ../public_html
-rm -rf ../public_html.old
+mv ../music.circleofhope.net ../music.circleofhope.net.old
+mv public_html ../music.circleofhope.net
+rm -rf ../music.circleofhope.net.old
 
 git checkout .
 popd
